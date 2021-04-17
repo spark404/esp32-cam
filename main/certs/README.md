e8fdc8ebea361d13d10a69e2b7f131c0ba156017
Hugo Trippaers <hugo@trippaers.nl>
2021-04-17 21:50:21 +0200

    Add the setup of a TLS connection to the AWS iot core
     * Note that certificates must be in the certs directory

---
+++ main/certs/README.md
@@ -0,0 +1,8 @@
+Certificates
+============
+
+Expected files
+
+ * aws-root-ca.pem  - A bundle with AWS root certificates (PEM)
+ * certificate.pem.crt  - Device certificate (PEM)
+ * private.pem.key - Device certificate private key (PEM)
--- main/main.c
+++ main/main.c
@@ -14,13 +14,28 @@
 #include "esp_log.h"
 #include "esp_vfs_fat.h"
 #include "driver/sdmmc_host.h"
+#include "esp_tls.h"
 
 #include "nvs.h"
 #include "nvs_flash.h"
 
+#include "sdkconfig.h"
+
 #define TAG "main"
 
 extern esp_err_t wifi_init();
+
+extern const uint8_t server_root_cert_pem_start[] asm("_binary_aws_root_ca_pem_start");
+extern const uint8_t server_root_cert_pem_end[]   asm("_binary_aws_root_ca_pem_end");
+
+extern const uint8_t client_crt_pem_start[]       asm("_binary_certificate_pem_crt_start");
+extern const uint8_t client_crt_pem_end[]         asm("_binary_certificate_pem_crt_end");
+extern const uint16_t client_crt_pem_length      asm("certificate_pem_crt_length");
+
+extern const uint8_t client_key_pem_start[]       asm("_binary_private_pem_key_start");
+extern const uint8_t client_key_pem_end[]         asm("_binary_private_pem_key_end");
+extern const uint16_t client_key_pem_length      asm("private_pem_key_length");
+
 
 void app_main()
 {
@@ -37,4 +52,25 @@ void app_main()
 
     // Initialize and connect WiFi
     wifi_init();
+
+    esp_tls_t *tls;
+    tls = esp_tls_init();
+
+    esp_tls_cfg_t esp_tls_cfg = {
+            .use_global_ca_store = false,
+            .cacert_buf = (const unsigned char *) server_root_cert_pem_start,
+            .cacert_bytes = server_root_cert_pem_end - server_root_cert_pem_start,
+            .clientcert_buf = (const unsigned char *) client_crt_pem_start,
+            .clientcert_bytes = client_crt_pem_end - client_crt_pem_start,
+            .clientkey_buf = (const unsigned char *) client_key_pem_start,
+            .clientkey_bytes = client_key_pem_end - client_key_pem_start
+    };
+    int result = esp_tls_conn_new_sync(CONFIG_ESPCAM_AWS_IOT_ENDPOINT, strlen(CONFIG_ESPCAM_AWS_IOT_ENDPOINT), 8883, &esp_tls_cfg, tls);
+    if (result != 1) {
+        ESP_LOGE(TAG, "esp_tls failed to connect");
+    }
+
+
+
+    esp_tls_conn_destroy(tls);
 }
