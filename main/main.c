1c81972580e8e919ab1d9cdf833d0ed3005f2fd2
Hugo Trippaers <hugo@trippaers.nl>
2021-04-18 11:24:47 +0200

    Add the camera component

--- main/main.c
+++ main/main.c
@@ -15,6 +15,7 @@
 #include "esp_vfs_fat.h"
 #include "driver/sdmmc_host.h"
 #include "esp_tls.h"
+#include "esp_camera.h"
 
 #include "nvs.h"
 #include "nvs_flash.h"
@@ -39,6 +40,56 @@ extern const uint16_t client_crt_pem_length      asm("certificate_pem_crt_length
 extern const uint8_t client_key_pem_start[]       asm("_binary_private_pem_key_start");
 extern const uint8_t client_key_pem_end[]         asm("_binary_private_pem_key_end");
 extern const uint16_t client_key_pem_length      asm("private_pem_key_length");
+
+#define CAM_PIN_PWDN    32 //power down is not used
+#define CAM_PIN_RESET   -1 //software reset will be performed
+#define CAM_PIN_XCLK     0
+#define CAM_PIN_SIOD    26
+#define CAM_PIN_SIOC    27
+
+#define CAM_PIN_D7      35
+#define CAM_PIN_D6      34
+#define CAM_PIN_D5      39
+#define CAM_PIN_D4      36
+#define CAM_PIN_D3      21
+#define CAM_PIN_D2      19
+#define CAM_PIN_D1      18
+#define CAM_PIN_D0       5
+#define CAM_PIN_VSYNC   25
+#define CAM_PIN_HREF    23
+#define CAM_PIN_PCLK    22
+
+static camera_config_t camera_config = {
+        .pin_pwdn  = CAM_PIN_PWDN,
+        .pin_reset = CAM_PIN_RESET,
+        .pin_xclk = CAM_PIN_XCLK,
+        .pin_sscb_sda = CAM_PIN_SIOD,
+        .pin_sscb_scl = CAM_PIN_SIOC,
+
+        .pin_d7 = CAM_PIN_D7,
+        .pin_d6 = CAM_PIN_D6,
+        .pin_d5 = CAM_PIN_D5,
+        .pin_d4 = CAM_PIN_D4,
+        .pin_d3 = CAM_PIN_D3,
+        .pin_d2 = CAM_PIN_D2,
+        .pin_d1 = CAM_PIN_D1,
+        .pin_d0 = CAM_PIN_D0,
+        .pin_vsync = CAM_PIN_VSYNC,
+        .pin_href = CAM_PIN_HREF,
+        .pin_pclk = CAM_PIN_PCLK,
+
+        //XCLK 20MHz or 10MHz for OV2640 double FPS (Experimental)
+        .xclk_freq_hz = 20000000,
+        .ledc_timer = LEDC_TIMER_0,
+        .ledc_channel = LEDC_CHANNEL_0,
+
+        .pixel_format = PIXFORMAT_JPEG,//YUV422,GRAYSCALE,RGB565,JPEG
+        .frame_size = FRAMESIZE_UXGA,//QQVGA-QXGA Do not use sizes above QVGA when not JPEG
+
+        .jpeg_quality = 12, //0-63 lower number means higher quality
+        .fb_count = 1 //if more than one, i2s runs in continuous mode. Use only with JPEG
+};
+
 
 void mqtt_callback(MQTTContext_t *pMqttContext, MQTTPacketInfo_t *pMqttPacketInfo, MQTTDeserializedInfo_t *pMqttDeserializedInfo) {
     ESP_LOGD(TAG, "mqtt_callback");
@@ -121,4 +172,10 @@ void app_main()
 
     MQTT_Disconnect(&pMqttContext);
     esp_tls_conn_destroy(tls);
+
+    err = esp_camera_init(&camera_config);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Camera Init Failed");
+    }
+
 }
